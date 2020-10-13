996e000c5ae492c3866956aaca8333edd3ea9d93
maxiflo88 <maxiflo88@users.noreply.github.com>
2020-10-13 08:43:49 +0300

    'changes'

--- lib/data/features/indicators.py
+++ lib/data/features/indicators.py
@@ -1,56 +1,50 @@
 import ta
-import pandas as pd
 
 
-diff = lambda x, y: x - y
-abs_diff = lambda x, y: abs(x - y)
+def add_indicators(df):
+    df2=df.copy()
+    dataset = ta.add_all_ta_features(df, 'Open', 'High', 'Low', 'Close', 'Volume BTC', fillna=True)
+    df2['RSI'] = dataset['momentum_rsi']
+    df2['MFI'] = dataset['momentum_mfi']
+    df2['TSI'] = dataset['momentum_tsi']
+    df2['UO'] = dataset['momentum_uo']
+    df2['AO'] = dataset['momentum_ao']
 
+    df2['MACD_diff'] = dataset['trend_macd_diff']
+    df2['Vortex_pos'] = dataset['trend_vortex_ind_pos']
+    df2['Vortex_neg'] = dataset['trend_vortex_ind_neg']
+    df2['Vortex_diff'] = dataset['trend_vortex_ind_diff']
+    df2['Trix'] = dataset['trend_trix']
+    df2['Mass_index'] = dataset['trend_mass_index']
+    df2['CCI'] = dataset['trend_cci']
+    df2['DPO'] = dataset['trend_dpo']
+    df2['KST'] = dataset['trend_kst']
+    df2['KST_sig'] = dataset['trend_kst_sig']
+    df2['KST_diff'] = dataset['trend_kst_diff']
+    df2['Aroon_up'] = dataset['trend_aroon_up']
+    df2['Aroon_down'] = dataset['trend_aroon_down']
+    df2['Aroon_ind'] = dataset['trend_aroon_ind']
 
-indicators = [
-    ('RSI', ta.rsi, ['Close']),
-    ('MFI', ta.money_flow_index, ['High', 'Low', 'Close', 'Volume BTC']),
-    ('TSI', ta.tsi, ['Close']),
-    ('UO', ta.uo, ['High', 'Low', 'Close']),
-    ('AO', ta.ao, ['High', 'Close']),
-    ('MACDDI', ta.macd_diff, ['Close']),
-    ('VIP', ta.vortex_indicator_pos, ['High', 'Low', 'Close']),
-    ('VIN', ta.vortex_indicator_neg, ['High', 'Low', 'Close']),
-    ('VIDIF', abs_diff, ['VIP', 'VIN']),
-    ('TRIX', ta.trix, ['Close']),
-    ('MI', ta.mass_index, ['High', 'Low']),
-    ('CCI', ta.cci, ['High', 'Low', 'Close']),
-    ('DPO', ta.dpo, ['Close']),
-    ('KST', ta.kst, ['Close']),
-    ('KSTS', ta.kst_sig, ['Close']),
-    ('KSTDI', diff, ['KST', 'KSTS']),
-    ('ARU', ta.aroon_up, ['Close']),
-    ('ARD', ta.aroon_down, ['Close']),
-    ('ARI', diff, ['ARU', 'ARD']),
-    ('BBH', ta.bollinger_hband, ['Close']),
-    ('BBL', ta.bollinger_lband, ['Close']),
-    ('BBM', ta.bollinger_mavg, ['Close']),
-    ('BBHI', ta.bollinger_hband_indicator, ['Close']),
-    ('BBLI', ta.bollinger_lband_indicator, ['Close']),
-    ('KCHI', ta.keltner_channel_hband_indicator, ['High', 'Low', 'Close']),
-    ('KCLI', ta.keltner_channel_lband_indicator, ['High', 'Low', 'Close']),
-    ('DCHI', ta.donchian_channel_hband_indicator, ['Close']),
-    ('DCLI', ta.donchian_channel_lband_indicator, ['Close']),
-    ('ADI', ta.acc_dist_index, ['High', 'Low', 'Close', 'Volume BTC']),
-    ('OBV', ta.on_balance_volume, ['Close', 'Volume BTC']),
-    ('CMF', ta.chaikin_money_flow, ['High', 'Low', 'Close', 'Volume BTC']),
-    ('FI', ta.force_index, ['Close', 'Volume BTC']),
-    ('EM', ta.ease_of_movement, ['High', 'Low', 'Close', 'Volume BTC']),
-    ('VPT', ta.volume_price_trend, ['Close', 'Volume BTC']),
-    ('NVI', ta.negative_volume_index, ['Close', 'Volume BTC']),
-    ('DR', ta.daily_return, ['Close']),
-    ('DLR', ta.daily_log_return, ['Close'])
-]
+    df2['BBH'] = dataset['volatility_bbh']
+    df2['BBL'] = dataset['volatility_bbl']
+    df2['BBM'] = dataset['volatility_bbm']
+    df2['BBHI'] = dataset['volatility_bbhi']
+    df2['BBLI'] = dataset['volatility_bbli']
+    df2['KCHI'] = dataset['volatility_kchi']
+    df2['KCLI'] = dataset['volatility_kcli']
+    df2['DCHI'] = dataset['volatility_dch']
+    df2['DCLI'] = dataset['volatility_dcl']
+    df2['ADI'] = dataset['volume_adi']
+    df2['OBV'] = dataset['volume_obv']
+    df2['CMF'] = dataset['volume_cmf']
+    df2['FI'] = dataset['volume_fi']
+    df2['EM'] = dataset['volume_em']
+    df2['VPT'] = dataset['volume_vpt']
+    df2['NVI'] = dataset['volume_nvi']
 
+    df2['DR'] = dataset['others_dr']
+    df2['DLR'] = dataset['others_dlr']
 
-def add_indicators(df) -> pd.DataFrame:
-    for name, f, arg_names in indicators:
-        wrapper = lambda func, args: func(*args)
-        args = [df[arg_name] for arg_name in arg_names]
-        df[name] = wrapper(f, args)
-    df.fillna(method='bfill', inplace=True)
-    return df
+    df2.fillna(method='bfill', inplace=True)
+
+    return df2
